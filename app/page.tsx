a9eb60654c6079e0f19ab8b51c4ae770d9aefc5f
BARAMEE RUJIPATANAPONG <baramee.rujipatanapong@gmail.com>
2024-11-04 21:07:39 +1100

    Add hideScrollbar class to globals.css and apply it to main container in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,14 @@ import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Footer from "@/components/ui/Footer";
 import { navItems } from "@/data";
+import "@/app/globals.css";
 
 export default function Home() {
   return (
-    <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+    <main
+      className={`relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5`}
+    >
+      <div className="max-w-7xl w-full hide-scrollbar">
         <FloatingNav navItems={navItems} />
         <Hero />
         <About />
